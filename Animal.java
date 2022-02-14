7bd8bf4568415c15f5b8e927bdc19fe2dcceaff4
jteresco <terescoj@gmail.com>
2022-02-14 14:15:13 -0500

    Indentation.

--- Animal.java
+++ Animal.java
@@ -1,50 +1,48 @@
 /**
-   Abstract class defining the required functionality of an Animal.
-
-   @author Jim Teresco
-   @version Spring 2020
-
-*/
+ * Abstract class defining the required functionality of an Animal.
+ * 
+ * @author Jim Teresco
+ * @version Spring 2022
+ */
 
 abstract public class Animal {
 
-    // the variable that was in all classes is moved into this abstract class
-    protected double weight;
+   // the variable that was in all classes is moved into this abstract class
+   protected double weight;
 
-    /**
-       Return the species of the type of animal.
-
-       @return the species of the type of animal.
+   /**
+    * Return the species of the type of animal.
+    * 
+    * @return the species of the type of animal.
     */
-    abstract public String species();
+   abstract public String species();
 
-    /**
-       Return the number of legs that members of this
-       species should have.
-
-       @return the number of legs that members of this
-       species should have
+   /**
+    * Return the number of legs that members of this
+    * species should have.
+    * 
+    * @return the number of legs that members of this
+    *         species should have
     */
-    abstract public int numLegs();
+   abstract public int numLegs();
 
-
-    /**
-       Set the weight of this animal
-       
-       @param newWeight the new weight of the animal
+   /**
+    * Set the weight of this animal
+    * 
+    * @param newWeight the new weight of the animal
     */
-    public void setWeight(double newWeight) {
+   public void setWeight(double newWeight) {
 
-	weight = newWeight;
-    }
+      weight = newWeight;
+   }
 
-    /**
-       Get the weight of the animal
-       
-       @return the weight of the animal
+   /**
+    * Get the weight of the animal
+    * 
+    * @return the weight of the animal
     */
-    public double getWeight() {
+   public double getWeight() {
 
-	return weight;
-    }
+      return weight;
+   }
 }
--- Dog.java
+++ Dog.java
@@ -1,68 +1,66 @@
 /**
-   Class defining a few characteristics of a dog.
-
-   @author Jim Teresco
-   @version Spring 2020
-
-*/
+ * Class defining a few characteristics of a dog.
+ * 
+ * @author Jim Teresco
+ * @version Spring 2022
+ * 
+ */
 
 public class Dog extends Animal {
 
     /**
-       Construct a new dog with the given weight
-
-       @param weight the initial weight of the dog
-    */
+     * Construct a new dog with the given weight
+     * 
+     * @param weight the initial weight of the dog
+     */
     public Dog(double weight) {
 
-	this.weight = weight;
+        this.weight = weight;
     }
-    
-    /**
-       Return the species: "dog"
 
-       @return the species "dog"
-    */
+    /**
+     * Return the species: "dog"
+     * 
+     * @return the species "dog"
+     */
     @Override
     public String species() {
 
-	return "dog";
+        return "dog";
     }
 
     /**
-       Return the number of legs that dogs have (4).
-
-       @return 4 for the number of legs a dog has
-    */
+     * Return the number of legs that dogs have (4).
+     * 
+     * @return 4 for the number of legs a dog has
+     */
     @Override
     public int numLegs() {
 
-	return 4;
+        return 4;
     }
 
     /**
-       Bark!
-    */
+     * Bark!
+     */
     public void bark() {
-	if (weight < 20) {
-	    System.out.println("Yip!");
-	}
-	else if (weight < 50) {
-	    System.out.println("Bark!");
-	}
-	else {
-	    System.out.println("Woof!");
-	}
+        if (weight < 20) {
+            System.out.println("Yip!");
+        } else if (weight < 50) {
+            System.out.println("Bark!");
+        } else {
+            System.out.println("Woof!");
+        }
     }
 
     /**
-       Build and return a String representation of the dog.
-
-       @return String representation of the dog
-    */
+     * Build and return a String representation of the dog.
+     * 
+     * @return String representation of the dog
+     */
     public String toString() {
 
-	return "Dog weighs " + weight + " pounds.";
+        return "Dog weighs " + weight + " pounds.";
     }
 
 }
